f55e0483f48aeca5c4adf90cd581dcda9bfad36b
鎌田正聖 <masatoshi@masa-macbook-pro.local>
2020-07-13 22:55:19 +0900

    done d

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -4,5 +4,21 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        n: usize,
+        k: usize,
+        p: [f64; n]
     }
+    let mut a = vec![0.0; n];
+    for i in 0..n {
+        a[i] = (p[i] + 1.0) / 2.0;
+    }
+    let mut sum = a.iter().take(k).sum::<f64>();
+    let mut ans = sum;
+    for j in k..n {
+        sum += a[j] - a[j - k];
+        if ans < sum {
+            ans = sum;
+        }
+    }
+    println!("{}", ans)
 }
